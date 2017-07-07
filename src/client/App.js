188ed58ebe4aca495ca34ac6c1ea81dc797d0aac
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2017-07-07 23:38:44 +0300

    render all comic books as id list

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,26 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default function App() {
-  return <div>Hello World from App!</div>
+class App extends React.PureComponent {
+  render() {
+    return (
+      <ul>
+        {this.renderComicBooks()}
+      </ul>
+    )
+  }
+
+  renderComicBooks() {
+    return this.props.comicBooks.map(comicBook => (
+      <li key={comicBook.id}>
+        ID: {comicBook.id}
+      </li>
+    ))
+  }
 }
+
+const mapStateToProps = state => ({
+  comicBooks: state.get('comicBooks').toJS(),
+})
+
+export default connect(mapStateToProps)(App)

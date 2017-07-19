61fee4c8f1b0c2c31b4e08dc959b41bea31a96a1
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2017-07-20 00:11:21 +0300

    Render comic books with more details in list

--- src/front-end/components/App.js
+++ src/front-end/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { List, ListItem } from 'material-ui/List'
 import AppBar from 'material-ui/AppBar'
+import Subheader from 'material-ui/Subheader'
 
 class App extends React.PureComponent {
   render() {
@@ -16,12 +17,40 @@ class App extends React.PureComponent {
   }
 
   renderComicBooks() {
-    return this.props.comicBooks.map(comicBook => (
-      <ListItem
-        key={comicBook.get('id')}
-        primaryText={comicBook.get('id')}
-      />
-    ))
+    let comicBooks = this.props.comicBooks.sortBy(
+      comicBook => comicBook.get('published')
+    )
+
+    let lastYear
+
+    return comicBooks.flatMap(comicBook => {
+      const year = comicBook.get('published').substr(0, 4)
+      const headerNeeded = year !== lastYear
+      lastYear = year
+
+      return [
+        this.renderHeader(year, headerNeeded),
+        <ListItem
+          key={comicBook.get('id')}
+          primaryText={this.renderMainInfo(comicBook)}
+        />,
+      ]
+    })
+  }
+
+  renderHeader(year, headerNeeded) {
+    if (headerNeeded) {
+      return (
+        <Subheader key={year}>{year}</Subheader>
+      )
+    }
+  }
+
+  renderMainInfo(comicBook) {
+    const name = comicBook.get('name')
+    const number = comicBook.get('number')
+    const published = comicBook.get('published')
+    return `${name} #${number} - ${published}`
   }
 }
 

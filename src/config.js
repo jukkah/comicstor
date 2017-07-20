57f86597848fda69d2ca3f6097ddaffb14bfe186
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2017-07-20 10:36:57 +0300

    Make all configs overridable by env vars

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 
 export const port = process.env.PORT || 3000
 
-export const enableGraphiQL = true;
+export const enableGraphiQL = process.env.ENABLE_GRAPHIQL || true
 
-export const logFile = 'store.log'
+export const logFile = process.env.LOG_FILE || 'store.log'
 
 // = App config ================================================================

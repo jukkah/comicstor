2faab97b15be4fe2c3d0b4232352453618a78b48
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2017-07-21 20:08:01 +0300

    Fix boolean env vars

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@
 
 export const port = process.env.PORT || 3000
 
-export const enableGraphiQL = process.env.ENABLE_GRAPHIQL || true
+export const enableGraphiQL = process.env.ENABLE_GRAPHIQL === 'true'
 
 export const logFile = process.env.LOG_FILE || 'store.log'
 
-export const enableLogFileExport = process.env.ENABLE_LOG_FILE_EXPORT || true
+export const enableLogFileExport = process.env.ENABLE_LOG_FILE_EXPORT === 'true'
 
-export const enableLogFileImport = process.env.ENABLE_LOG_FILE_IMPORT || true
+export const enableLogFileImport = process.env.ENABLE_LOG_FILE_IMPORT === 'true'
 
 // = App config ================================================================

48c8ce23d52c3d53f338eb64d075f0bcb38f832c
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2017-09-05 22:21:28 +0300

    Add details to item page

--- src/client/components/ItemPage.js
+++ src/client/components/ItemPage.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { List, ListItem } from 'material-ui/List'
+import Chip from 'material-ui/Chip'
 import AppBar from 'material-ui/AppBar'
 import Subheader from 'material-ui/Subheader'
+import Avatar from 'material-ui/Avatar'
 import IconButton from 'material-ui/IconButton'
 import NavigationArrowBack from 'material-ui/svg-icons/navigation/arrow-back'
 import { withRouter } from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'
 
 class ItemPage extends React.PureComponent {
+  static defaultProps = {
+    condition: 10,
+    removed: true,
+  }
+
   handleBack = () => {
    this.props.history.goBack()
   };
@@ -20,12 +29,128 @@ class ItemPage extends React.PureComponent {
           iconElementLeft={<IconButton><NavigationArrowBack /></IconButton>}
           onLeftIconButtonTouchTap={this.handleBack}
         />
+        {this.renderDetails()}
       </div>
     )
   }
 
   renderTitle() {
-    return `${this.props.name} #${this.props.number} - ${this.props.published}`
+    const name = this.props.name
+    const number = this.props.number
+    const year = moment(this.props.published).format('YYYY')
+    return `${name} #${number}/${year}`
+  }
+
+  renderDetails() {
+    return (
+      <List>
+        {this.renderName()}
+        {this.renderNumber()}
+        {this.renderPublished()}
+        {this.renderOriginalPrice()}
+        {this.renderPurchasePrice()}
+        {this.renderCondition()}
+        {this.renderTags()}
+      </List>
+    )
+  }
+
+  renderName() {
+    if (this.props.name) {
+      return (
+        <ListItem
+          primaryText={this.props.name}
+          secondaryText="Nimi"
+        />
+      )
+    }
+  }
+
+  renderNumber() {
+    if (this.props.number) {
+      return (
+        <ListItem
+          primaryText={this.props.number}
+          secondaryText="Numero"
+        />
+      )
+    }
+  }
+
+  renderPublished() {
+    if (this.props.published) {
+      return (
+        <ListItem
+          primaryText={moment(this.props.published).format('D.M.YYYY')}
+          secondaryText="Julkaisupäivä"
+        />
+      )
+    }
+  }
+
+  renderOriginalPrice() {
+    if (this.props.originalPrice) {
+      const value = numeral(this.props.originalPrice.value).format('0,0.00')
+      const code = this.props.originalPrice.code
+      return (
+        <ListItem
+          primaryText={`${value} ${code}`}
+          secondaryText="Irtonumeron hinta"
+        />
+      )
+    }
+  }
+
+  renderPurchasePrice() {
+    if (this.props.purchasePrice) {
+      const value = numeral(this.props.purchasePrice.value).format('0,0.00')
+      const code = this.props.purchasePrice.code
+      return (
+        <ListItem
+          primaryText={`${value} ${code}`}
+          secondaryText="Ostohinta"
+        />
+      )
+    }
+  }
+
+  renderCondition() {
+    if (this.props.condition) {
+      return (
+        <ListItem
+          primaryText={`${this.props.condition}/10`}
+          secondaryText="Kunto"
+        />
+      )
+    }
+  }
+
+  renderTags() {
+    if (this.props.tags.length || this.props.removed) {
+      const chips = [
+        ...this.props.tags.map(this.renderTag),
+        this.renderTag('Poistettu'),
+      ]
+
+      return (
+        <ListItem
+          primaryText={
+            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+              {chips}
+            </div>
+          }
+        />
+      )
+    }
+  }
+
+  renderTag(tag) {
+    return (
+      <Chip key={tag} style={{ margin: 4 }}>
+        <Avatar size={32}>{tag.charAt(0).toUpperCase()}</Avatar>
+        {tag}
+      </Chip>
+    )
   }
 }
 

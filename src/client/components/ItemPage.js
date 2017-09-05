b5e8875c8c239c89a083d16005fb698605ca9127
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2017-09-05 19:19:18 +0300

    Go back when back button is checked in item page

--- src/client/components/ItemPage.js
+++ src/client/components/ItemPage.js
@@ -3,14 +3,29 @@ import { connect } from 'react-redux'
 import { List, ListItem } from 'material-ui/List'
 import AppBar from 'material-ui/AppBar'
 import Subheader from 'material-ui/Subheader'
+import IconButton from 'material-ui/IconButton'
+import NavigationArrowBack from 'material-ui/svg-icons/navigation/arrow-back'
+import { withRouter } from 'react-router-dom'
 
 class ItemPage extends React.PureComponent {
+  handleBack = () => {
+   this.props.history.goBack()
+  };
+
   render() {
     return (
       <div>
-        <AppBar />
+        <AppBar
+          title={this.renderTitle()}
+          iconElementLeft={<IconButton><NavigationArrowBack /></IconButton>}
+          onLeftIconButtonTouchTap={this.handleBack}
+        />
       </div>
     )
+  }
+
+  renderTitle() {
+    return `${this.props.name} #${this.props.number} - ${this.props.published}`
   }
 }
 
@@ -22,4 +37,4 @@ const mapStateToProps = (state, props) => {
   return result ? result.toJS() : {}
 }
 
-export default connect(mapStateToProps)(ItemPage)
+export default withRouter(connect(mapStateToProps)(ItemPage))
